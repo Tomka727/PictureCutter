9a88ea9193ed4a90398947e95bff901d7aeb1a9b
Tomas Šveikauskis <60540453+Tomka727@users.noreply.github.com>
2022-07-06 11:48:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,23 +10,29 @@ data = asar(image)
 image2 = Img.fromarray(data)
 
 line_size = cfg.line_size
-print(data.shape)
+
+# horizontal slicing of image
 data1 = data[0:line_size:]
 data2 = data[line_size:line_size*2:]
+
 for x in range(2, int(data.shape[0]/line_size)):
     if x % 2 == 0:
         data1 = np.concatenate((data1, data[x*line_size:(x+1)*line_size:]), dtype='uint8')
     else:
         data2 = np.concatenate((data2, data[x * line_size:(x + 1) * line_size:]), dtype='uint8')
+
 data = np.concatenate((data1, data2), dtype='uint8')
 
+# vertical slicing of image
 data1 = data[::, 0:line_size:]
 data2 = data[::, line_size:line_size*2:]
+
 for x in range(2, int(data.shape[1]/line_size)):
     if x % 2 == 0:
         data1 = np.concatenate((data1, data[::, x*line_size:(x+1)*line_size:]), axis=1, dtype='uint8')
     else:
         data2 = np.concatenate((data2, data[::, x * line_size:(x + 1) * line_size:]), axis=1, dtype='uint8')
+
 data = np.concatenate((data1, data2), axis=1, dtype='uint8')
 
 img = Img.fromarray(data, 'RGB')
